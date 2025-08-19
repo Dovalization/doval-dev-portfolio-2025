40777566112145aa599295ad7ef5e3517d752808
Guilherme Doval <doval.guilherme94@gmail.com>
2025-08-19 20:43:09 -0300

    refactor ContactCTASection component; improve text clarity and consistency

--- src/components/contact-cta-section.tsx
+++ src/components/contact-cta-section.tsx
@@ -8,13 +8,13 @@ export default function ContactCTASection() {
           {`Let's build something great together`}
         </h2>
         <p className="text-gray-light mx-auto mb-8 max-w-prose text-xl leading-relaxed">
-          {`If you have a project in mind or just want to say hello, feel free to reach out! I'm always open to discussing new ideas, collaborations, or opportunities.`}
+          {`If you have a project in mind or just want to say hello, reach out. I’m always open to new ideas, collaborations, and opportunities.`}
         </p>
         <Link
           href="/contact"
           className="bg-orange-secondary text-dark-primary hover:bg-orange-primary inline-block transform rounded-full px-12 py-4 text-lg font-bold shadow-lg transition-all duration-300 hover:scale-105 hover:shadow-xl"
         >
-          Get In Touch
+          Get in touch
         </Link>
       </div>
     </section>
